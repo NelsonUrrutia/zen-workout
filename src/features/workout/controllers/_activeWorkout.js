7bda1305bbec086e8286c653bfcd054a458b2e94
Nelson E. Urrutia <nelurrutia15@gmail.com>
2024-02-15 13:32:48 -0600

    Timer | Raw solution

--- src/features/workout/controllers/_activeWorkout.js
+++ src/features/workout/controllers/_activeWorkout.js
@@ -55,7 +55,6 @@ function startWorkoutHandler() {
 }
 
 function mainTimerFunctions() {
-  // FIXME: INFINITE LOOP
   const {
     sets,
     restingTimeSetsMinutes,
@@ -72,11 +71,83 @@ function mainTimerFunctions() {
   );
 
   let counterSetRestingTime = setsRestingTimeSeconds;
-  let counterSet = 0;
+  let counterSet = 1;
 
   let counterBlockWorkingTime = workingTimeBlocksSeconds;
   let counterBlockRestingTime = restingTimeBlocksSeconds;
   let counterBlock = 0;
+
+  while (counterSet <= sets) {
+    // Decrease block's timer
+    counterBlockWorkingTime--;
+    console.log(
+      `Seconds ${counterBlockWorkingTime} | Current Exercise 💪🏻 ${
+        blockSettings.at(counterBlock).name
+      }`
+    );
+
+    // Check if block's timer counter finished
+    if (counterBlockWorkingTime === 0) {
+      // Increase block's counter
+      counterBlock++;
+
+      // Reset to default block's timer
+      counterBlockWorkingTime = workingTimeBlocksSeconds;
+      console.log(`Block #${counterBlock} finished`);
+
+      // Check  the lock's counter to start resting timer
+      // If the block's counter is equal to blocks, it means
+      // the set is completed, and should start the set's resting timer
+      if (counterBlock < blocks) {
+        while (counterBlockRestingTime >= 0) {
+          // Decrease block's resting timer
+          counterBlockRestingTime--;
+          console.log(`Seconds ${counterBlockRestingTime} | Resting 🫁 `);
+
+          // Check if the block's resting timer finished
+          if (counterBlockRestingTime === 0) {
+            // Set to default block's resting timer
+            counterBlockRestingTime = restingTimeBlocksSeconds;
+
+            // End the loop
+            break;
+          }
+        }
+      }
+    }
+
+    // Check if the block's counter is equal to the blocks
+    if (counterBlock === blocks) {
+      // Reset block's counter
+      counterBlock = 0;
+      console.log(`Set #${counterSet} finished`);
+      console.log("--------------------");
+
+      // Check  the set's counter to start resting timer
+      // If the set's counter is equal to sets, it means
+      // the workout session is completed.
+      if (counterSet < sets) {
+        while (counterSetRestingTime >= 0) {
+          // Decrease set's resting timer
+          counterSetRestingTime--;
+          console.log(`🫁🫁 ${counterSetRestingTime}`);
+
+          // Check if the set's resting timer finished
+          if (counterSetRestingTime === 0) {
+            // Set to default set's resting timer
+            counterSetRestingTime = setsRestingTimeSeconds;
+            console.log("--------------------");
+
+            // End the loop
+            break;
+          }
+        }
+      }
+
+      // Increase set's counter
+      counterSet++;
+    }
+  }
 }
 
 export default function initActiveWorkoutModule() {
